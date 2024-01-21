448c46d179d3e4ac4a5ac4840772d6beec188792
ZephyrZ923 <156784600+ZephyrZ923@users.noreply.github.com>
2024-01-21 13:51:48 +0000

    Add files via upload

---
+++ toggle_light.py
@@ -0,0 +1,37 @@
+
+print(bool(""))
+
+
+
+
+
+
+def add(num1, num2):
+    return num1 + num2
+# return 可以让电脑知道 further things
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def test():
+    pass
+
+print(test())
+
+
+
+
+
+
+
+
+
+
+
+
