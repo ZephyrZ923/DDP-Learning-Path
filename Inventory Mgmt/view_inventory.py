0b75b18ba4876d987bc2d68c04ccc915c1a94c96
ZephyrZ923 <156784600+ZephyrZ923@users.noreply.github.com>
2024-01-21 14:02:44 +0000

    Python exercise

---
+++ Inventory Mgmt/view_inventory.py
@@ -0,0 +1,15 @@
+
+inventory = {"Laptops": 10, "Monitors": 15}
+
+
+def view_inventory():
+    # Implementation Instructions:
+    # 1. Loop through the inventory dictionary.
+    # 2. Print each item's name and its quantity.
+    for item, quantity in inventory.items():
+        print(f'{item}: {quantity}')
+
+
+view_inventory() 
+
+   
